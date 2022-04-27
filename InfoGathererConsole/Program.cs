1ff060e734dd5433f90df94e5bb50f0b9a371329
Carlo Mercuri <cme@td-k.dk>
2022-04-27 14:24:03 +0200

    Add project files.

---
+++ InfoGathererConsole/Program.cs
@@ -0,0 +1,43 @@
+﻿using InfoGathererConsole.Control;
+using System;
+using System.Diagnostics;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Security;
+using System.Text;
+
+namespace InfoGathererConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GathererMain();
+        }
+
+        static void GathererMain()
+        {
+            while(true)
+            {
+                Console.WriteLine("Select which information you would like displayed: ");
+                Console.WriteLine("1 - Local Ping");
+                Console.WriteLine("2 - Hostname");
+                Console.WriteLine("3 - Traceroute");
+                Console.WriteLine("4 - DHCP");
+                Console.WriteLine("5 - Local Machine");
+
+
+            }
+
+            Console.ReadKey();
+
+        }
+
+        
+
+        
+
+       
+    }
+}
